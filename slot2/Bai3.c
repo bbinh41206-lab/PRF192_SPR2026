58775ac9805ecf1252d9ccad51dd7866bd7b4de0
bbinh41206-lab <bbinh41206@gmail.com>
2026-01-09 11:48:26 +0700

    bai3

---
+++ slot2/Bai3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+float main(){
+	float a = 5, b = 10.216;
+	
+	float ketqua = a + b;
+	
+	printf("a=%.0f\n", a);
+	
+	printf("b=%.3f\n", b);
+	
+	printf("%.0f + %.3f = %.2f , a, b, ketqua");
+	
+	return 0;
+	
+	
+}
