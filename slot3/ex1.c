462b252a70d64f4eb6391aa4c00c6554174b65bb
bbinh41206-lab <bbinh41206@gmail.com>
2026-01-14 08:16:15 +0700

    Up ex1.c leen forder

---
+++ slot3/ex1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+	int a = 7;
+	int b = 2;
+	printf("a + b = %d\n", a + b);
+	printf("a - b = %d\n", a - b);
+	printf("a  * b = %d\n", a * b);
+	printf("a / b = %.2f", (float)a / b);
+	return 0;
+
+
+}
